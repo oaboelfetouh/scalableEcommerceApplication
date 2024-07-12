47355ecc5d18456e16c869553561d759d24b9a64
oaboelfetouh <oaboelfetouh@gmail.com>
2024-07-12 15:37:34 +0300

    Omar's test

--- products/app.js
+++ products/app.js
@@ -1,1 +1,15 @@
-app = require('express')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
