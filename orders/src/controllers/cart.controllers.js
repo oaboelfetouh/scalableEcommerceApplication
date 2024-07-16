57f09ee45f4da88925536d2bc85bbc7ca6d4df7c
ahmeddkhamiess10 <ahmeddkhamiess10@gmail.com>
2024-07-16 12:10:55 +0300

    add the functions in controllers
    Signed-off-by: ahmeddkhamiess10 <ahmeddkhamiess10@gmail.com>

---
+++ orders/src/controllers/cart.controllers.js
@@ -0,0 +1,15 @@
+const addItemToCart = async (req,res)=>{
+    // ---- to be done ----
+}
+const deleteItemFromCart = async (req,res)=>{
+    // ---- to be done ----
+}
+const viewCart = async (req,res)=>{               // retreive only the product details in the cart
+    // ---- to be done ----
+}
+const checkout = async (req,res)=>{               // retreive the total price of the cart and user info (address , contact phone , ......)
+    // ---- to be done ---- 
+}
+const confirmPayment = async (req,res)=>{         // check balance and confirm order
+    // ---- to be done ----
+}
--- orders/src/controllers/orders.controllers.js
+++ orders/src/controllers/orders.controllers.js
@@ -1,3 +1,13 @@
-const createOrder = (req,res)=>{
+const createOrder = async (req,res)=>{
+    // ---- to be done ----
+}
+
+
+const deleteOrder = async (req,res)=>{
+    // ---- to be done ----
+}
+
+
+const getOrders = async (req,res)=>{             // history of orders of users with id
     // ---- to be done ----
 }
