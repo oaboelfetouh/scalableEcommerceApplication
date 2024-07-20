2dc80abf153b718c04a54bdd0cae25360eb329de
Ahmed Khamis <ahmeddkhamiess10@gmail.com>
2024-07-20 13:46:27 +0300

    test-models
    Signed-off-by: Ahmed Khamis <ahmeddkhamiess10@gmail.com>

---
+++ orders/src/models/orders.models.js
@@ -0,0 +1,49 @@
+const { db } = require("../database");
+const DataTypes = require("sequelize");
+
+
+
+// User model
+const User = db.define("User", {
+   userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true
+   },
+   firstName: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   lastName: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   email: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     validate: {
+       isEmail: true
+     }
+   },
+   password: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   phoneNumber: {
+     type: DataTypes.STRING(15),
+     allowNull: false
+   }
+   
+  
+ }, {
+   timestamps: false,
+   tableName: "User"
+ });
+ 
+ 
+ 
+ 
+ 
+ module.exports = { User };
+ 
